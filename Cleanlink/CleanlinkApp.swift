bd9b27e76e078746e781ad47eef24e548718219b
Abdullah Ziauddin <abdullahibneziauddin13@gmail.com>
2026-03-13 00:35:37 +0300

    Initial Commit

---
+++ Cleanlink/CleanlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanlinkApp.swift
+//  Cleanlink
+//
+//  Created by Abdullah Ziauddin on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
